16e9019164ff6efc97867c1231bb801916f93a57
Amer Alkhatib <157820866+Amerak15@users.noreply.github.com>
2024-02-02 14:28:21 +0100

    adding js file

---
+++ script.js
@@ -0,0 +1,72 @@
+//1.1
+/* var myAge=22;
+if (myAge>=18) {
+    console.log("You are eligibe for voting")
+} else {
+    console.log("You don't meet the voting requirments")
+} 
+/////
+//1.2
+let name=prompt("What's your name?");
+if (name==="John") {
+    console.log("Hello,John!")
+} else {
+    console.log("You are not John!")
+}
+////
+//2.1
+function num(num1,num2) {
+    return num1+num2;    
+}
+let sum=num(10,5);
+console.log(sum);
+////
+//2.2
+function reverseString(string){
+    var splitString=str.split();
+
+    var reverseArray=splitString.reverse();
+
+    var joinArray=reverseArray.Join();
+    return joinArray;
+} 
+////
+///3.1
+var myFruits=["banana","strawberry","peach","apple","kiwi"]
+for (let i = 0; i < myFruits.length; i++) {
+    const element = myFruits[i];
+    console.log(myFruits[i])
+}
+////
+//3.2
+const avg =function(array) {
+    let total=0;
+    for (let i = 0; i < array.length; i++) {
+        total+=array[i];
+        const average=sum/array.length;
+        console.log(average);
+        return average;
+    }
+}
+/////
+//3.3
+function largest(array1){
+    let max=array1[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array1[i]>max) {
+            max=array1[i];
+        }
+        return max;
+    }
+}*/
+////
+//3.4
+const wordsArr = ["the", "kid", "ate", "the", "apple"];
+let result = "";
+for (let i = 0; i < wordsArr.length; i++) {
+result += wordsArr[i] + " ";
+}
+console.log(result);
+
+
+
